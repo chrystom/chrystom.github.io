66ace5262cfddd75372d1509f0cb174d7d923873
chrystom <chrystomt@gmail.com>
2023-05-29 23:43:06 +1000

    added playground, maybe folder functionality later

---
+++ playground.js
@@ -0,0 +1,9 @@
+// NOTE: below comment is now obsolete since I actually decided to program
+//actual programming? imagine. mark-up languages are the only languages i speak.
+
+let clicky = document.querySelector("#clicky");
+
+// maybe some folder functionality in the near future???
+clicky.addEventListener("dblclick", (event) => {
+    console.log("double-clicked");
+});
