5392c9aaf18169c0833785c26ecb05171f935d46
chrystom (flido) <chrystomt@gmail.com>
2023-05-30 11:31:40 +1000

    added folder and folder highlight class + event listener

--- js/playground.js
+++ js/playground.js
@@ -1,9 +1,21 @@
 // NOTE: below comment is now obsolete since I actually decided to program
 //actual programming? imagine. mark-up languages are the only languages i speak.
 
-let clicky = document.querySelector("#clicky");
+let folder = document.querySelector("#folder");
+let fldHighlight = false;
 
 // maybe some folder functionality in the near future???
-clicky.addEventListener("dblclick", (event) => {
-    console.log("double-clicked");
+folder.addEventListener("click", () => {
+    console.log("clicked");
+    if (fldHighlight) {
+        folder.classList.remove("single-clicked");
+        fldHighlight = false;
+    } else {
+        folder.classList.add("single-clicked");
+        fldHighlight = true;
+    }
 });
+
+// folder.addEventListener("dblclick", (event) => {
+//     console.log("double-clicked");
+// });
